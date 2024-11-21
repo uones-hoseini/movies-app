30a44d1733acbf1756752def060d4018081c317f
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-11-21 17:56:03 +0330

    transform images

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const MovieList = ({ movies }) => {
   return (
-    <div className='image-container d-flex justify-content-star m-3'>
+    <>
       {movies.map((movie, index) => (
-        <img key={index} src={movie.Poster} alt="Poster" />
+        <div className='image-container d-flex justify-content-star m-3'>
+        <img key={index} src={movie.Poster} alt="Movie" />
+        <div className='overly d-flex align-items-center justify-content-center'></div>
+        </div>
       ))}
-    </div>
+    </>
   );
 }
 
