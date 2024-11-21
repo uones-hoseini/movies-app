746fea173a4a0869e6693d2d8d846a102e0e9d33
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-11-21 18:14:35 +0330

    style to MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,9 +4,9 @@ const MovieList = ({ movies }) => {
   return (
     <>
       {movies.map((movie, index) => (
-        <div className='image-container d-flex justify-content-star m-3'>
+        <div className='image-container d-flex justify-content-center '>
         <img key={index} src={movie.Poster} alt="Movie" />
-        <div className='overly d-flex align-items-center justify-content-center'></div>
+        <div className='overly d-flex align-items-center justify-content-center m-3'></div>
         </div>
       ))}
     </>
